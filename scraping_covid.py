7785a010ed9883058069e0596537cd4500709910
Ubuntu <ubuntu@ip-172-31-24-55.ap-southeast-1.compute.internal>
2021-04-25 05:46:49 +0000

    Move Server

--- scraping_covid.py
+++ scraping_covid.py
@@ -1,7 +1,8 @@
-from psycopg2 import connect
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, MetaData
 from requests import get
 from bs4 import BeautifulSoup
-from datetime import datetime
 from datetime import datetime
 import time
 import pytz
@@ -13,28 +14,39 @@ url2 = 'https://ddc.moph.go.th/viralpneumonia/index.php'
 response = get(url)
 response2 = get(url2)
 soup = BeautifulSoup(response2.text, 'html.parser')
-new = '+' + soup.find('div',{'class':'mybg1'}).find('h4').text
+new_cases = soup.find('div',{'class':'mybg1'}).find('h4').text
 html_soup = BeautifulSoup(response.text,'html.parser')
 data = html_soup.find_all('span')
-confirm_casses = data[4].text
-dead = data[5].text
-recovery = data[6].text
+confirm_cases = data[4].text
+dead_cases = data[5].text
+recovery_cases = data[6].text
 
-def insert_todb(last_update, confirm_casses, recovery, dead, new):
-    conn = connect(host='18.136.100.9',
-                    database='smarthome',
-                    user='postgres',
-                    password='bu12345',
-                    port='5432')
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO covid_update (last_update,confirm_case,recovery,dead,new) VALUES (%s,%s,%s,%s,%s)",(last_update, confirm_casses, recovery, dead, new))
-    conn.commit()
-    print(cursor.rowcount, "Record inserted successfully into sub table")
-    cursor.close()
-  
-            
-insert_todb(date_time,confirm_casses,recovery,dead,new)
-print('confirm casses: ',confirm_casses)
-print('dead: ',dead)
-print('recovery: ', recovery)
-print('new: ',new)
+db_string = "postgresql://postgres:bu12345@18.136.100.9:5432/smarthome"
+db = create_engine(db_string)
+
+meta = MetaData(db)
+covid_table = Table('covid_updates', meta,
+                Column('last_update'),
+                Column('confirm_case'),
+                Column('recovery'),
+                Column('dead'),
+                Column('new'),
+                Column('createdAt'),
+                Column('updatedAt'))
+
+with db.connect() as conn:
+    select_statement = covid_table.select()
+    insert_statement = covid_table.insert().values(last_update=date_time,
+                                                   confirm_case=confirm_cases,
+                                                   recovery=recovery_cases,
+                                                   dead=dead_cases,
+                                                   new=new_cases,
+                                                   createdAt=date_time,
+                                                   updatedAt=date_time)
+
+    conn.execute(insert_statement)   
+
+print('confirm casses: ',confirm_cases)
+print('dead: ',dead_cases)
+print('recovery: ', recovery_cases)
+print('new: ',new_cases)
